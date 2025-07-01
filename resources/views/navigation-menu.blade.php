554abf9de42a43ebf029fef890a649f6ee889c4b
Babi Sobrinho <79843387+babisobrinho@users.noreply.github.com>
2025-07-01 13:02:39 +0100

    light mode/dark mode

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -108,14 +108,18 @@
                                 </x-dropdown-link>
                             @endif
 
+                            <!-- Theme Toggle -->
+                            <x-dropdown-link @click="toggleTheme()">
+                                <span x-text="darkMode ? 'Modo Claro' : 'Modo Escuro'"></span>
+                            </x-dropdown-link>
+
                             <div class="border-t border-gray-200 dark:border-gray-600"></div>
 
                             <!-- Authentication -->
                             <form method="POST" action="{{ route('logout') }}" x-data>
                                 @csrf
 
-                                <x-dropdown-link href="{{ route('logout') }}"
-                                         @click.prevent="$root.submit();">
+                                <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
                                     {{ __('Log Out') }}
                                 </x-dropdown-link>
                             </form>
@@ -171,12 +175,17 @@
                     </x-responsive-nav-link>
                 @endif
 
+                <!-- Theme Toggle -->
+                <x-responsive-nav-link @click="toggleTheme()">
+                    <span x-text="darkMode ? 'Modo Claro' : 'Modo Escuro'"></span>
+                </x-responsive-nav-link>
+
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}" x-data>
                     @csrf
 
                     <x-responsive-nav-link href="{{ route('logout') }}"
-                                   @click.prevent="$root.submit();">
+                        @click.prevent="$root.submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
