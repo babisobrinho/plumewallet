17fda3f7f67907767997c3978fa40783c59639e3
Lenice Soares <contatolenicesoares@gmail.com>
2025-10-08 19:13:27 +0100

    update botão sem uso backoffice

--- resources/views/backoffice/layouts/app.blade.php
+++ resources/views/backoffice/layouts/app.blade.php
@@ -141,13 +141,6 @@
                     </div>
                     
                     <div class="flex items-center space-x-4">
-                        <!-- Notifications -->
-                        <button class="p-2 text-gray-400 hover:text-gray-600">
-                            <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-5 5v-5zM11 19H6a2 2 0 01-2-2V7a2 2 0 012-2h5m0 0V3a2 2 0 012-2h2a2 2 0 012 2v2m-4 0V3a2 2 0 012-2h2a2 2 0 012 2v2m-4 0h4"></path>
-                            </svg>
-                        </button>
-                        
                         <!-- User Menu -->
                         <div class="relative">
                             <div class="relative" x-data="{ open: false }">
